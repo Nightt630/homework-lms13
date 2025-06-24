79bc917f67a445f4f9ccd1cf4c190d94eb73f3d4
Nightt630 <artemribakov6@gmail.com>
2025-06-24 15:36:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+const numbers = [1, 2, 3, 4];
+
+numbers.forEach(() => {
+  console.log(numbers[0] * 2, numbers[1] * 2, numbers[2] * 2, numbers[3] * 2);
+});
+
+
+
+const pricesUAH = [80, 120, 200];
+
+pricesUAH.forEach(() => {
+  console.log(pricesUAH[0] * 41,pricesUAH[1] * 41,pricesUAH[2] * 41)
+});
+
+const priceDolars = [3280, 4920, 8200]
+console.log(priceDolars);
+
+
+
+const number = [5, 12, 8, 20, 3];
+console.log(number[1], number[3]);
+
+
+const names = ["Олег", "Анна", "Іван", "Артем"];
+names[0] = "Артем";
+
+
+
+names[3] = "Олег";
+console.log(names)
+
+
+
+const subjects = ["математика", "українська мова", "хімія"];
